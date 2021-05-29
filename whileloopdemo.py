9ada9e8e610fe1f52ba9305f5f44f2fc3ba846da
rahul-tope31 <jyoti_g15@rediffmail.com>
2021-05-29 23:13:30 +0530

    change in coding of whileloop.py of printing pattern      on date 29-05-2021

--- whileloopdemo.py
+++ whileloopdemo.py
@@ -1,5 +1,39 @@
 i = 1
-while i <= 10:
-  print(i)
-  i = i +2
-  
+while i <= 5:
+  j = 1
+  while j <= i:
+    print("* ",end="")
+    j = j + 1
+  print("\r")
+  i = i +1
+print("Another 180 rotation pattern")
+
+# Function to demonstrate printing pattern
+def pypart2(n):
+  # number of spaces
+  k = 2 * n - 2
+
+  # outer loop to handle number of rows
+  for i in range(0, n):
+
+    # inner loop to handle number spaces
+    # values changing acc. to requirement
+    for j in range(0, k):
+      print(end=" ")
+
+    # decrementing k after each loop
+    k = k - 2
+
+    # inner loop to handle number of columns
+    # values changing acc. to outer loop
+    for j in range(0, i + 1):
+      # printing stars
+      print("* ", end="")
+
+    # ending line after each row
+    print("\r")
+
+
+# Driver Code
+n = 5
+pypart2(n)
