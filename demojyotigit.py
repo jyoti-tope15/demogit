d210ec31b990ed6179dc64aa40f33d9b3a7beea3
rahul-tope31 <jyoti_g15@rediffmail.com>
2021-05-29 22:39:35 +0530

    change in coding of evenodd.py on date 29-05-2021

--- demojyotigit.py
+++ demojyotigit.py
@@ -1,4 +1,18 @@
+
+
 print("Hello jyoti")
 print("Welcome to git ")
 print("hi")
 print("how r u?")
+
+i=1
+for i in range(0,5):
+    j=1
+    for j in range(0,i+1):
+        print("* ",end="" )
+    print("\r")
+
+
+
+
+
