e058262e1b5286ae393e41324cf01d9d888c9c69
jyoti-tope15 <84035480+jyoti-tope15@users.noreply.github.com>
2021-06-03 22:48:41 +0530

    pizza recipe

---
+++ Recipe.py
@@ -0,0 +1,13 @@
+
+
+print(" I like Pizza")
+print("Recipe are done as :-")
+print(" 1. take pizza base")
+print(" 2. apply miyonnize over it")
+print(" 3. cut capsicum, onion, tomato and other need vegetables u want")
+print(" 4. put all vegetables over the pizza base")
+print(" 5. put some tomato sauce over it")
+print(" 6. then put cheese over it ")
+print(" 7. finally put that pizza in tray ")
+print(" 8. and put it into microwave oven to bake for 2 to 3 min")
+
